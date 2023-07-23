735d0277a5e62c5831e7ced8a533af3575c2280e
Amadeusz Klimaszewski <amadeusz.klimaszewski@gmail.com>
2023-07-23 17:14:11 +0200

    Move model config above field definitions in AppModel class

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -5,7 +5,8 @@ from pydantic import BaseModel, ConfigDict, Field
 
 
 class AppModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: UUID = Field(default_factory=uuid4)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
-    model_config = ConfigDict(from_attributes=True)
